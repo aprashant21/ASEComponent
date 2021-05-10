9d9fbfcfb3578124963212bcc9463c7d60f82c42
aprashant21 <aprashant21@tbc.edu.np>
2021-05-10 16:02:33 +0545

    Circle Class Added

---
+++ ASEComponent/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ASEComponent
+{
+    class Circle : IShapes
+    {
+        public int x, y, radius;
+
+        public Circle() : base()
+        {
+        }
+        public Circle(int x, int y, int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void draw(Graphics g)
+        {
+            try
+            {
+                Pen p = new Pen(Color.Yellow, 2);
+                g.DrawEllipse(p, x - radius, y - radius, radius * 2, radius * 2);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void set(Color c, params int[] list)
+        {
+            try
+            {
+                this.x = list[0];
+                this.y = list[1];
+                this.radius = list[2];
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
--- ASEComponent/ShapesFactory.cs
+++ ASEComponent/ShapesFactory.cs
@@ -10,7 +10,27 @@ namespace ASEComponent
     {
         public override IShapes getShape(string ShapeType)
         {
-            
+            ShapeType = ShapeType.ToLower().Trim();
+            if (ShapeType.Equals("circle"))
+            {
+                return new Circle();
+            }
+            else if (ShapeType.Equals("rectangle"))
+            {
+                return new Rectangle();
+            }
+
+            else if (ShapeType.Equals("triangle"))
+            {
+                return new Triangle();
+            }
+
+            else
+            {
+                //throw an appropriate exception.
+                System.ArgumentException argEx = new System.ArgumentException("Factory error: " + ShapeType + " does not exist");
+                throw argEx;
+            }
         }
     }
 }
